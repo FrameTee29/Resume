e1350a8b5ee366ab96f0aa4d25d9e404a620b21b
FrameTee29 <teeraphat123@hotmail.com>
2021-01-04 18:43:55 +0700

    :hammer: navbar scss

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 const Navbar = () => {
+
+    const [statusmenu , setStatusmenu] = useState(false)
+
     return (
         <div className="header-topbar">
             <img src="/static/images/profile.jpg" alt="" className="logo" />
@@ -12,9 +17,14 @@ const Navbar = () => {
             </nav>
 
             <nav className="navbar-mobile">
-                <i className="fas fa-bars"></i>
-                <div>
-                    
+                <i className="fas fa-bars" onClick={()=>setStatusmenu(!statusmenu)}></i>
+                <div className={`list-mobile ${statusmenu ? "" : "close"}`}>
+                    <ul>
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Education</a></li>
+                        <li><a href="#">Skill</a></li>
+                        <li><a href="#">Achievement</a></li>
+                    </ul>
                 </div>
             </nav>
         </div>
